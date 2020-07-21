3f2d48c0ccfebe063511e6aed001daec8fd9c01f
o-henry <cksgod16@gmail.com>
2020-07-21 13:08:04 +0900

    Server : Delete passport

--- server/src/api/controllers/user.controller.ts
+++ server/src/api/controllers/user.controller.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response, NextFunction } from "express";
-import passport from "passport";
 
 import { UserService } from "@services/user.services";
 import { User } from "@models/User";
@@ -10,13 +9,5 @@ const route = Router();
 export default (app: Router) => {
   app.use(route);
 
-  route.get(
-    "/login",
-    passport.authenticate("kakao", {
-      failureRedirect: "/",
-    }),
-    (req: Request, res: Response, next: NextFunction) => {
-      res.redirect("/");
-    }
-  );
+  route.get("/login");
 };
