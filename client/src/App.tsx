d474641e6149c4247fd33214771b5f2d41ad2f41
o-henry <cksgod16@gmail.com>
2020-07-21 13:09:22 +0900

    Client : Delete kakao api code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { Main, Login } from "@/pages";
-import { kakaoAPI } from "@/utils/kakaoAPI";
 
 const App: React.FC = () => {
-  const kakao = kakaoAPI();
-
-  useEffect(() => {
-    kakao.initKakao();
-  });
-
   return (
     <>
       <Router>
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from "react";
-import { LoginTemplate } from "@/pages";
-import { kakaoAPI } from "@/utils/kakaoAPI";
-import { useStores } from "@/hooks";
 import { observer } from "mobx-react";
+
+import { LoginTemplate } from "@/pages";
+import { useStores } from "@/hooks";
 
 const Login = observer(() => {
   const { event } = useStores();
-  const kakao = kakaoAPI();
-
-  useEffect(() => {
-    event.isClicked && kakao.loginKakao();
-  }, [event.isClicked]);
 
   return (
     <>
