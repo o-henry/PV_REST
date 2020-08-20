209ca9af8a44a71c323ad4050ae6697b50318164
o-henry <cksgod16@gmail.com>
2020-08-20 22:23:37 +0900

    Client : Add Signup compo

--- client/src/pages/SignUp/Template/index.tsx
+++ client/src/pages/SignUp/Template/index.tsx
@@ -1,41 +1,89 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
+import Grid from "@material-ui/core/Grid";
 
-const SignUpTemplate = () => {
+const SignUpTemplate = (): React.ReactElement => {
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data: any) => console.log(data);
   console.log(errors);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="이름" name="이름" ref={register} />
-      <input
-        type="number"
-        placeholder="나이"
-        name="나이"
-        ref={register({ required: true, max: 99, min: 0, maxLength: 2 })}
-      />
+    <div className="signup">
+      <Grid container>
+        <Grid item xs={12}>
+          <div className="signup form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <input
+                  required
+                  className="input"
+                  type="text"
+                  placeholder="ID"
+                  name="이름"
+                  ref={register({
+                    required: true,
+                  })}
+                />
+              </div>
+              <div>
+                <input
+                  required
+                  className="input"
+                  type="number"
+                  placeholder="나이"
+                  name="나이"
+                  ref={register({
+                    required: true,
+                    max: 99,
+                    min: 0,
+                    maxLength: 2,
+                  })}
+                />
+              </div>
+              <div className="signup radio">
+                <div className="signup radio inner">
+                  <input
+                    className="radio button"
+                    required
+                    name="성별"
+                    type="radio"
+                    value="Male"
+                    ref={register({ required: true })}
+                  />
+                  <label htmlFor="Male">여성</label>
+                </div>
+                <div className="signup radio inner">
+                  <input
+                    className="radio button"
+                    required
+                    name="성별"
+                    type="radio"
+                    value="Female"
+                    ref={register({ required: true })}
+                  />
+                  <label htmlFor="Female">남성</label>
+                </div>
+              </div>
+              <div>
+                <input
+                  required
+                  className="input"
+                  type="password"
+                  placeholder="비밀번호"
+                  name="비밀번호"
+                  ref={register({
+                    required: true,
+                  })}
+                />
+              </div>
 
-      <input
-        name="성별"
-        type="radio"
-        value="Male"
-        ref={register({ required: true })}
-      />
-      <input
-        name="성별"
-        type="radio"
-        value="Female"
-        ref={register({ required: true })}
-      />
-
-      <input type="submit" />
-    </form>
+              <input type="submit" />
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
