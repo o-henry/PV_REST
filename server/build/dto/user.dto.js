47bca253480191636024ed8b20ca284283ea8dcc
o-henry <cksgod16@gmail.com>
2020-10-05 14:49:37 +0900

    Add NODE_ENV

--- server/build/dto/user.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateKakaoUser = void 0;
-class CreateKakaoUser {
-}
-exports.CreateKakaoUser = CreateKakaoUser;
-//# sourceMappingURL=user.dto.js.map
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = "production" || "development";
 
 const envFound = dotenv.config();
 
